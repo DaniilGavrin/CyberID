974921fbafaae0e6323f5a85a5eb058d794f61c5
Daniil <80905799+DaniilGavrin@users.noreply.github.com>
2026-03-29 22:35:18 +0300

    Updates

--- _next/static/FVZsPwKNRzZD4ruEW7CEX/_buildManifest.js
+++ _next/static/FVZsPwKNRzZD4ruEW7CEX/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/CyberID//_next/:path+",
+        "destination": "/CyberID/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
